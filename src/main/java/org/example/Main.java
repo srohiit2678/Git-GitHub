c3d448ce2c840c0deaf6d369c1ae468b1298ed99
srohiit2678 <rohitsikhwal99@gmail.com>
2025-12-29 17:03:17 +0530

    change in loop to 15

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,8 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 15; i++) {
             System.out.println("i = " + i);
         }
     }
