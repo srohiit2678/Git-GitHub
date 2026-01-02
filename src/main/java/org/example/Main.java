9a90e15ed6e7ab9c549a53f4895a1650d9c5dec5
srohiit2678 <rohitsikhwal99@gmail.com>
2026-01-02 09:36:12 +0530

    change i*2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-      // change and pull from github
+
         for (int i = 1; i <= 15; i++) {
-            System.out.println("i = " + i);
+
+            System.out.println("i = " + i*2);
         }
     }
 }
