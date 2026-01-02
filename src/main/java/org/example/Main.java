15f6bb65f0388ab2b57ace9ee844c2e0e4484085
srohiit2678 <rohitsikhwal99@gmail.com>
2026-01-02 11:35:08 +0530

    change itreation to 10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-      // change and pull from github
+
         for (int i = 1; i <= 15; i++) {
             System.out.println("i = " + i);
         }
