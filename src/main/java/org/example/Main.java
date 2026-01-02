959c019ec072b11f2ace4e91d4f95e6d9f16a43e
srohiit2678 <rohitsikhwal99@gmail.com>
2026-01-02 12:21:19 +0530

    remove all

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-
-=======
->>>>>>> 693e3c3845b31b430d62f577dc7f5eb631484234
-        for (int i = 1; i <= 15; i++) {
-
+      for (int i = 1; i <= 15; i++) {
             System.out.println("i = " + i*2);
         }
     }
