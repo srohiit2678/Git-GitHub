1ea0bcee940549925207abf51b982a393d738b1e
srohiit2678 <rohitsikhwal99@gmail.com>
2026-01-02 11:43:19 +0530

    new line added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
+        // loop itration
         for (int i = 1; i <= 15; i++) {
             System.out.println("i = " + i);
         }
